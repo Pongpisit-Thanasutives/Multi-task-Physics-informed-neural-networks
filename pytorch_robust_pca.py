91bc8870cd939b93a952e32c0492e754e03250b9
Pongpisit-Thanasutives <pongpisit.tha@gmail.com>
2021-07-23 18:49:17 +0900

    some codes

--- pytorch_robust_pca.py
+++ pytorch_robust_pca.py
@@ -4,6 +4,65 @@ from torch import nn
 import torch.nn.functional as F
 from torch.linalg import norm as Norm
 
+class R_pca_numpy:
+    def __init__(self, D, mu=None, lmbda=None):
+        self.D = D
+        self.S = np.zeros(self.D.shape)
+        self.Y = np.zeros(self.D.shape)
+
+        if mu:
+            self.mu = mu
+        else:
+            self.mu = np.prod(self.D.shape) / (4 * np.linalg.norm(self.D, ord=1))
+
+        self.mu_inv = 1 / self.mu
+
+        if lmbda:
+            self.lmbda = lmbda
+        else:
+            self.lmbda = 1 / np.sqrt(np.max(self.D.shape))
+
+    @staticmethod
+    def frobenius_norm(M):
+        return np.linalg.norm(M, ord='fro')
+
+    @staticmethod
+    def shrink(M, tau):
+        return np.sign(M) * np.maximum((np.abs(M) - tau), np.zeros(M.shape))
+
+    def svd_threshold(self, M, tau):
+        U, S, V = np.linalg.svd(M, full_matrices=False)
+        return np.dot(U, np.dot(np.diag(self.shrink(S, tau)), V))
+
+    def fit(self, tol=None, max_iter=1000, iter_print=100):
+        iter = 0
+        err = np.Inf
+        Sk = self.S
+        Yk = self.Y
+        Lk = np.zeros(self.D.shape)
+
+        if tol:
+            _tol = tol
+        else:
+            _tol = 1E-7 * self.frobenius_norm(self.D)
+
+        #this loop implements the principal component pursuit (PCP) algorithm
+        #located in the table on page 29 of https://arxiv.org/pdf/0912.3599.pdf
+        while (err > _tol) and iter < max_iter:
+            Lk = self.svd_threshold(
+                self.D - Sk + self.mu_inv * Yk, self.mu_inv)                            #this line implements step 3
+            Sk = self.shrink(
+                self.D - Lk + (self.mu_inv * Yk), self.mu_inv * self.lmbda)             #this line implements step 4
+            Yk = Yk + self.mu * (self.D - Lk - Sk)                                      #this line implements step 5
+            err = self.frobenius_norm(self.D - Lk - Sk)
+            iter += 1
+            if (iter % iter_print) == 0 or iter == 1 or iter > max_iter or err <= _tol:
+                print('iteration: {0}, error: {1}'.format(iter, err))
+
+        self.L = Lk
+        self.S = Sk
+        return Lk, Sk
+
 # This module has 0 param, and act like a function
 class R_pca(nn.Module):
     def __init__(self, D, mu=None, lmbda=None, verbose=False):
