8f6cc569d58b7e20645e19f12c60c07bea1d3455
Pongpisit-Thanasutives <pongpisit.tha@gmail.com>
2021-09-06 18:16:40 +0900

    some codes

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 rm -rf ./__pycache__
 rm -rf ./.ipynb_checkpoints
 rm -rf ./.virtual_documents
-rm -rf /Users/pongpisit/Library/Caches/pip
+rm -rf /Users/pongpisit/Library/Caches/pip/
 
 FILE=./kite_tutorial.ipynb
 if test -f "$FILE"; then
